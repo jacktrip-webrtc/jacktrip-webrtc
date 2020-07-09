f34ec0e90618a2302289478288a97017d0c484d7
Matteo Sacchetto <matteosacchetto1996@gmail.com>
2020-07-09 13:12:54 +0200

    Updated home page replacing axios with fetch

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -2,19 +2,26 @@
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
+
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 function createRoom() {
-  axios.post('/room')
-    .then(function (response) {
-      let room = response.data;
-      let url = room.data.url
-
-      window.location.assign(url);
+    fetch('/room', {
+        method: 'POST'
     })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
+    .then((response) => {
+        // Examine the JSON in the response
+        return response.json();
+    })
+    .then((data) => {
+        let room = data;
+        let url = room.data.url
+
+        window.location.assign(url);
+    })
+    .catch((error) => {
+        // handle error
+        console.error(error);
     })
 }
